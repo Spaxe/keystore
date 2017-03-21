e278cdc508021f1d469115b84532811f28a77dd8
Xavier Ho <contact@xavierho.com>
2017-03-21 22:33:19 +1100

    Updated to v0.1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name = 'keystore',
     packages = ['keystore'],
-    version = '0.1.1',
+    version = '0.1.2',
     description = 'Compress and encrypt your private keys',
     author = 'Xavier Ho',
     author_email = 'contact@xavierho.com',
